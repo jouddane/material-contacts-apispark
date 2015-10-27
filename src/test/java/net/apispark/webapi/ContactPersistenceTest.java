3a8715f6f6ba89ac45cdaca44ddafa223fe2ff31
jouddane <jouddane@gmail.com>
2015-10-27 16:08:13 +0100

    Tests implémentés #1

---
+++ src/test/java/net/apispark/webapi/ContactPersistenceTest.java
@@ -0,0 +1,29 @@
+package net.apispark.webapi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.apispark.webapi.db.ContactPersistence;
+import net.apispark.webapi.representation.Contact;
+import net.apispark.webapi.representation.Gender;
+
+public class ContactPersistenceTest {
+	@Test
+	public void TryContactId(){
+		Contact c = new Contact(null, "Micael", "Mbagira", "svg-1", Gender.MALE);
+		c=ContactPersistence.INSTANCE.addContact(c);
+		Assert.assertNotNull(c.getId());
+		ContactPersistence.INSTANCE.deleteContact(c.getId());
+	}
+	@Test
+	public void GetContactWithId(){
+		Contact c = new Contact(null, "Micaelle", "Mbagira", "svg-6", Gender.FEMALE);
+		c=ContactPersistence.INSTANCE.addContact(c);
+		Contact d = new Contact();
+		d=ContactPersistence.INSTANCE.getContact(c.getId());
+		equals(d.getId()==c.getId());
+		
+	}
+}
